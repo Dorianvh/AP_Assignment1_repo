26f414a618a3f2f16cc808235195b2d3da462b56
ThijscS <thijs.suurmond@gmail.com>
2021-09-24 14:34:12 +0200

    print set

--- src/ap1.java
+++ src/ap1.java
@@ -1,36 +1,52 @@
 import java.util.Scanner;
+import java.io.PrintStream;
 
 public class ap1 {
 
-    Scanner input = new Scanner(System.in).useDelimiter("");
-    Set firstSet = new Set();
-    Set secondSet = new Set();
-    Identifier a = new Identifier();
-
-    //print statement met getElement en removeElement
-
-    void start(){
-        try {
-            firstSet.addElement(a);
-        } catch (Exception e){
-
-        }
+    static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    PrintStream out;
 
 
-        /*
-        char c = nextChar(input);
-        if (c != '}') {
-            firstSet.identifierArray[0].add(c);
-        }
-        System.out.println(firstSet.identifierArray[0].print());
+    boolean askSet (Scanner input, String question, Set set) {
+        do {out.printf("%s", question);
+            if (! input.hasNextLine()) {
+                out.printf("\n"); // otherwise line with ^D will be overwritten
+                return false;
+            }
+        } while (! inputContainsCorrectSet(input, set));
+        return true;
+    }
 
-         */
+    boolean askBothSets (Scanner input, Set set1, Set set2) {
+        return askSet(input, "Give first set : ", set1) && askSet(input, "Give second set : ", set2);
     }
 
     char nextChar (Scanner in) {
-            return in.next().charAt(0);
+        return in.next().charAt(0);
     }
 
+    boolean inputContainsCorrectSet(Scanner input, Set set) throws Exception{
+        //check for {} and correct " " delimeters
+        input.useDelimiter("");
+        if (nextChar(input) != '{') {
+            throw new Exception("Set needs to starts with: { ");
+            return false;
+        }
+
+    }
+
+    Set calculateAndGiveOutput(Set set1, Set set2){
+        //give union etc
+    }
+
+    void start () {
+        Scanner in = new Scanner(System.in);
+        Set set1 = new Set();
+        Set set2 = new Set();
+        while (askBothSets(in, set1, set2)) {
+            calculateAndGiveOutput(set1, set2);
+        }
+    }
     public static void main(String[] args) {
         new ap1().start();
     }
